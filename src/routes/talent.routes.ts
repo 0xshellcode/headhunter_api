78732aafa32ea6750cccaeb24844a1fd4f7ae865
0xshellc0de <david.r0sas@protonmail.com>
2021-11-18 22:39:43 -0600

    Fix talent route

--- src/routes/talent.routes.ts
+++ src/routes/talent.routes.ts
@@ -1,9 +1,9 @@
 import { Router } from 'express';
-import { signIn, signUp } from '../controllers/headhunter.controller';
+import { signIn, signUp } from '../controllers/talent.controller';
 
 const router = Router();
 
-router.post('/headhunter/signup', signUp);
-router.post('/headhunter/signin', signIn);
+router.post('/talent/signup', signUp);
+router.post('/talent/signin', signIn);
 
 export default router;
